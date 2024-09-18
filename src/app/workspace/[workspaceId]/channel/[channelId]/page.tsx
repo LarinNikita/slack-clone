cd8512655041401ace524145f381beb93d3aa264
LarinNikita <larin_nikita_m@mail.ru>
2024-09-18 15:27:00 +0700

    channel header ui. edit and delete functionality

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,5 +1,44 @@
+'use client';
+
+import { Loader, TriangleAlert } from 'lucide-react';
+
+import { useGetChannel } from '@/features/channels/api/use-get-channel';
+
+import { useChannelId } from '@/hooks/use-channel-id';
+
+import { Header } from './header';
+
 const ChannelIdPage = () => {
-    return <div>Channel Id Page</div>;
+    const channelId = useChannelId();
+
+    const { data: channel, isLoading: channelLoading } = useGetChannel({
+        id: channelId,
+    });
+
+    if (channelLoading) {
+        return (
+            <div className="flex h-full flex-1 items-center justify-center">
+                <Loader className="text-muted-foreground size-5 animate-spin" />
+            </div>
+        );
+    }
+
+    if (!channel) {
+        return (
+            <div className="flex h-full flex-1 flex-col items-center justify-center gap-y-2">
+                <TriangleAlert className="text-muted-foreground size-5" />
+                <span className="text-muted-foreground text-sm">
+                    Channel not found
+                </span>
+            </div>
+        );
+    }
+
+    return (
+        <div className="flex h-full flex-col">
+            <Header title={channel.name} />
+        </div>
+    );
 };
 
 export default ChannelIdPage;
